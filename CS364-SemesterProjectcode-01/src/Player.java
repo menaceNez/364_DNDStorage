d653a1d558c2b5f96f092f9876419aaa2920ce1e
LilyEngen27 <lilyengen@gmail.com>
2025-03-28 14:57:28 -0500

    adding player class with info

---
+++ CS364-SemesterProjectcode-01/src/Player.java
@@ -0,0 +1,32 @@
+public class Player {
+
+    private String FirstName; 
+    private String LastName; 
+    private int PlayID; 
+
+    public Player(String first, String last){
+        this.FirstName = first; 
+        this.LastName = last;  
+    }
+
+    public String getFirstName(){
+        return FirstName; 
+    }
+
+    public String getLastName(){
+        return LastName; 
+    }
+
+    public void setFirstName(String first){
+        this.FirstName = first; 
+    }
+
+    public void setLastName(String last){
+        this.LastName = last; 
+    }
+
+    public int getPlayID(){
+        return this.PlayID; 
+    }
+
+}
