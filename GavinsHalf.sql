9b564d386a77acbf084f5f831973c809cff75505
LilyEngen27 <lilyengen@gmail.com>
2025-03-28 13:02:09 -0500

    adding player tables

--- GavinsHalf.sql
+++ GavinsHalf.sql
@@ -20,3 +20,42 @@ CREATE TABLE PlaySession (
     EndTime DATETIME,
     Event VARCHAR(1024) -- might need larger to allow ample description
 );
+
+
+CREATE TABLE Player (
+	PlayID int NOT NULL AUTO_INCREMENT,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    PRIMARY KEY (PlayID)
+);
+
+CREATE TABLE Race(
+	RaceID int NOT NULL AUTO_INCREMENT, 
+    RaceName varchar(255),
+    PRIMARY KEY (RaceID)
+);
+
+CREATE TABLE Class(
+	ClassID int NOT NULL AUTO_INCREMENT, 
+    ClassName varchar(255),
+    PRIMARY KEY (ClassID)
+);
+
+
+CREATE TABLE  Persona(
+	CharID int NOT NULL AUTO_INCREMENT,
+    PlayID int NOT NULL, 
+    ClassID int NOT NULL, 
+    RaceID int NOT NULL, 
+    CharLastName varchar(255) NOT NULL,
+    CharMiddName varChar(255), 
+	CharFirstName varchar(255),
+    Lev int NOT NULL, 
+    CreationDate Date Not Null, 
+
+    PRIMARY KEY (CharID), 
+    FOREIGN KEY (PlayID) REFERENCES Player(PlayID),
+    FOREIGN KEY (ClassID) REFERENCES Class(ClassID), 
+    FOREIGN KEY (RaceID) REFERENCES Race(RaceID)
+
+);
