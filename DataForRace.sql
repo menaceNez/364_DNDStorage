803dbbce68915bef920dfb36a6cfb56572fdf5a6
LilyEngen27 <lilyengen@gmail.com>
2025-03-31 18:28:13 -0500

    Update DataForRace.sql
    added data from SQL to show was data was added to the Race class

--- DataForRace.sql
+++ DataForRace.sql
@@ -1,1 +1,26 @@
+INSERT INTO Race(RaceName) 
+	VALUES('Dwarf'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Elf'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Halfling'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Human'); 
 
+INSERT INTO Race(RaceName) 
+	VALUES('Dragonborn'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Gnome'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Half-Elf'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Half-Orc'); 
+    
+INSERT INTO Race(RaceName) 
+	VALUES('Tiefling'); 
