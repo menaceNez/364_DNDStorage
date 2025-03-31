49953694c4b6cc588304d5c99eaf6063eca25024
LilyEngen27 <lilyengen@gmail.com>
2025-03-31 18:20:41 -0500

    Update DataForClass.sql with Classes
    added SQL that shows which classes where added to the Class table

--- DataForClass.sql
+++ DataForClass.sql
@@ -1,1 +1,32 @@
+INSERT INTO Class(ClassName)
+	VALUES('Barbarian'); 
 
+INSERT INTO Class(ClassName)
+	VALUES('Bard'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Druid'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Fighter'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Monk'); 
+
+INSERT INTO Class(ClassName)
+	VALUES('Paladin');
+    
+INSERT INTO Class(ClassName)
+	VALUES('Ranger'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Rogue'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Sorcerer'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Warlock'); 
+    
+INSERT INTO Class(ClassName)
+	VALUES('Wizard'); 
