d009c066d72506037bd92f56a0641d049025981d
LilyEngen27 <lilyengen@gmail.com>
2025-03-31 15:25:58 -0500

    created race objectclass

--- CS364-SemesterProjectcode-01/src/Race.java
+++ CS364-SemesterProjectcode-01/src/Race.java
@@ -1,3 +1,26 @@
 public class Race {
 
+    public int raceID; 
+    public String raceName; 
+
+    public  Race(String race){
+        this.raceName = race; 
+    }
+
+    public String getRaceName(){
+        return raceName; 
+    }
+
+    public void setRaceName(String race){
+        this.raceName = race; 
+    }
+
+    public void setRaceID(int ID){
+        this.raceID = ID; 
+    }
+
+    public int getRaceID(){
+        return raceID; 
+    }
+
 }
